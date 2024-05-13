1b4395ff22043c45c3c078055c2cdae0d9aa4b3f
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-05-13 22:08:46 +0400

    init

---
+++ index.js
@@ -0,0 +1,24 @@
+const http = require("http");
+
+const server = http.createServer(async (req, res) => {
+    if (req.url === '/' && req.method === 'GET') {
+        res.writeHead(200, {
+            'Content-Type': 'application/json'
+        });
+        res.write(JSON.stringify({
+            "message": "Hello"
+        }));
+        res.end();
+        return
+    }
+    res.writeHead(404, {
+        'Content-Type': 'application/json'
+    });
+    res.end(JSON.stringify({"message": "not found"}));
+});
+
+
+
+server.listen(3002, () => {
+    console.log("Server started on port 3002")
+})
